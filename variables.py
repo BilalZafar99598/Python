4bf31cbcc77c1adcf562dbfccea6058c40583168
BilalZafar99598 <bilal.halfur@gmail.com>
2022-01-06 23:06:43 +0500

    Python Variables

---
+++ variables.py
@@ -0,0 +1,30 @@
+a = 5
+b = 10
+# print(a+b)
+
+# print(a+20)
+
+_ = 50
+# print(_)
+
+name = 'Bilal Zafar'
+# print(name)
+fname = 'Bilal'
+lname = 'Zafar'
+fullName = fname + lname
+fullNameWithSpace = fname + " " + lname
+# print(fullName)
+print(fullNameWithSpace)
+num = '1234567890'
+# print(fullName[0])
+# print(fullName[1])
+# print(fullName[2])
+# print(fullName[3])
+# print(fullName[4])
+# print(fullName[5])
+# print(fullName[-1])
+# print(fullName[:5])
+print(num)
+print(num[::-3])
+print(num[::3])
+print(len(num))
