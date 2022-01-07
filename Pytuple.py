eaf467b80d345fa2ade0b5b7c9b917653f93c575
BilalZafar99598 <bilal.halfur@gmail.com>
2022-01-07 21:00:39 +0500

    Tuple and Dictionary Python

---
+++ Pytuple.py
@@ -0,0 +1,5 @@
+tup = (1,2,3,4,5,6,7,8,9,10)
+# print(tup)
+# print(type(tup))
+print(tup[::-1])
+print(t)
---
+++ dictionary.py
@@ -0,0 +1,18 @@
+students = {"Name":
+                ["Ali"
+                    ,"Omer"
+                    ,"Khan"],
+            "Age":[
+                20,
+                19,
+                23],
+                "Gender":[
+                        "Male",
+                        "Male",
+                        "Male"]}
+
+# print(students.keys())
+# print(students.values())
+# print(students)
+print(students["Gender"][:2])
+print(students.get("Gender"))
